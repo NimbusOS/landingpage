07c03220caff9074a4aa994d8d23a44603e84f64
Zhaowei <sunzhaow0813@gmail.com>
2023-01-09 15:50:16 -0500

    Added content

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Hero = () =>  {
+    return (
+        <div >
+           <h3>Hero</h3>
+           <button>Download</button>
+        </div>
+    );
+}
+
+export default Hero;
--- src/components/KeyFeatures.jsx
+++ src/components/KeyFeatures.jsx
@@ -5,7 +5,15 @@ const KeyFeatures = () =>  {
     return (
         <div className="KeyFeatures">
            <h3>Key Features</h3>
-           <p>Nimbus is a free-to-use, open-source serverless monitoring tool that allows you to manage your Lambda functions in real time and access valuable insights through a customizable dashboard. Unlike other tools, Nimbus is free to use and open source.</p>
+           <ul>
+            <li>Easily link your AWS account to nimbus in a secure and hassle-free process.</li>
+            <li>Our home page provides valuable insights into the health of your AWS application, with a focus on lambda functions.</li>
+            <li>You can easily switch between dark mode and light mode, depending on your preference.</li>
+            <li>The Functions tab offers a detailed breakdown of metrics for each of your functions, including invocations, errors, throttles, and durations.</li>
+            <li>Need to find specific logs? The Logs tab is where you can filter through all of your lambda function logs by time period, reports only, errors only, or any keyword.</li>
+            <li>The APIs tab gives you an in-depth look at common API metrics, endpoints, and the lambda functions they're connected to.</li>
+            <li>In the Settings tab, you can also update your personal information and login details with ease.</li>
+           </ul>
         </div>
     );
 }
