ba207ca6bc9d85f5d289d47f5f6a2872917045a6
Arturo Kim <arturokim96@gmail.com>
2023-01-14 14:20:18 -0500

    Updates to landing page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () =>  {
             <div className="hero-content flex-col lg:flex-row">
                 <div className="text-center">
                 <h3>The ultimate monitoring and visualization tool for AWS Lambda. </h3>    
-                <button className="btn flex items-center"><p>Download</p><img src={mac} alt="Mac Download" className="w-8"/></button>
+                {/* <button className="btn flex items-center"><p>Download</p><img src={mac} alt="Mac Download" className="w-8"/></button> */}
                 </div>
                 {/* Replace svg with whatever image we want here */}
                 <img src={Login} alt="login" className="rounded-xl w-3/5"/>
